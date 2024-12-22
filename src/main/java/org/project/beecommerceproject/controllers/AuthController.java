f8acc9673399b0e0995f3797dd86cd5a003fc686
truongleminhnghia <adronghia@gmail.com>
2024-12-22 10:07:38 +0700

    nghiatlm [update] add README.md

--- src/main/java/org/project/beecommerceproject/controllers/AuthController.java
+++ src/main/java/org/project/beecommerceproject/controllers/AuthController.java
@@ -4,15 +4,10 @@ import org.project.beecommerceproject.configs.CustomerUserDetail;
 import org.project.beecommerceproject.dtos.requests.LoginRequest;
 import org.project.beecommerceproject.dtos.requests.UserRegisterRequest;
 import org.project.beecommerceproject.dtos.responses.ApiResponse;
-import org.project.beecommerceproject.entities.Role;
 import org.project.beecommerceproject.entities.User;
-import org.project.beecommerceproject.enums.EnumRoleName;
-import org.project.beecommerceproject.enums.EnumStatusUser;
 import org.project.beecommerceproject.enums.EnumTypeLogin;
 import org.project.beecommerceproject.mappers.UserMapper;
-import org.project.beecommerceproject.repositories.UserRepository;
 import org.project.beecommerceproject.services.JwtService;
-import org.project.beecommerceproject.services.RoleService;
 import org.project.beecommerceproject.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -28,24 +22,12 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     @Autowired
     private AuthenticationManager authenticationManager;
-
     @Autowired
     private JwtService jwtService;
-
     @Autowired
     private UserService userService;
-
-    @Autowired
-    private UserRepository userRepository;
-
     @Autowired
     private UserMapper userMapper;
-
-    @Autowired
-    private RoleService roleService;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
     @PostMapping("/login")
     public ResponseEntity<ApiResponse> login(@RequestBody LoginRequest request, @RequestParam(value = "type_login", required = false, defaultValue = "TYPE_LOCAL") String type) {
